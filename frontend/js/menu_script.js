5a457700d3182c94819d82f7649f476bb12dbfbc
Theoharis Lampsas <156580714+teolamm@users.noreply.github.com>
2024-04-19 21:04:15 +0300

    Add files via upload

--- frontend/js/menu_script.js
+++ frontend/js/menu_script.js
@@ -1,7 +1,75 @@
-function showMenu(category){
+document.addEventListener('DOMContentLoaded', function() {
 
-}
+    document.querySelectorAll('.menu').forEach(button => {
+        button.addEventListener('click', function(){
+            let prevChosenSubcat = document.querySelector('#chosen');
+            prevChosenSubcat.removeAttribute("id");
 
-function selectItem(item){
+            button.setAttribute("id", "chosen");
+            
+            document.querySelector('.chosen_menu').textContent = button.textContent;
+            //κώδικας για εναλλαγή των παραθύρων του menu
+        })
+    })
 
-}
+    document.querySelectorAll('.item').forEach(div => {
+        div.addEventListener('click', function selectSize(event){
+            
+            let itemName = event.target.closest('.text-content');
+            itemName = itemName.textContent;
+
+            let amount = document.createElement('div');
+            amount.className = 'amount';
+
+            let chosen = document.querySelector('#chosen');
+            if (chosen.innerHTML == 'Coffee'){
+                let par = document.createElement('p');
+                par.classList = 'par';
+                par.innerHTML = 'size:';
+                let size = document.createElement('div');
+                size.className = 'size';
+                let small = document.createElement('button');
+                small.innerHTML = 'S';
+                small.setAttribute('id', 'set');
+                small.className = 'sizeButton'
+                let medium = document.createElement('button');
+                medium.innerHTML = 'M';
+                medium.className = 'sizeButton';
+                let large = document.createElement('button');
+                large.innerHTML = 'L';
+                large.className = 'sizeButton';
+                size.appendChild(large);
+                size.appendChild(medium);
+                size.appendChild(small);
+                amount.appendChild(par);
+                amount.appendChild(size);
+                div.removeEventListener('click', selectSize);
+                div.appendChild(amount);
+            } 
+            let countAndConfirm = document.createElement('div');
+            countAndConfirm.className = 'countAndConfirm';
+            let counter = document.createElement('div');
+            counter.className = 'counter';
+            let plus = document.createElement('button');
+            plus.className = 'operations';
+            plus.innerHTML = '+';
+            let minus = document.createElement('button');
+            minus.innerHTML = '-';
+            minus.className = 'operations';
+            let number = document.createElement('p');
+            number.innerHTML = '1';
+            number.className = 'operations';
+            counter.appendChild(plus);
+            counter.appendChild(number);
+            counter.appendChild(minus);
+            let confirm = document.createElement('button');
+            confirm.className = 'confirm';
+            confirm.innerHTML = 'Add to Basket';
+
+            countAndConfirm.appendChild(counter);
+            countAndConfirm.appendChild(confirm);
+            
+            div.appendChild(countAndConfirm);
+        })
+    })
+});
