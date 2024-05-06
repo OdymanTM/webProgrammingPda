80b3d13d578aa4ed3b22666503db698259769909
Odyman <palamaris02@gmail.com>
2024-05-06 16:22:12 +0300

    Add table model to handle database operations

---
+++ models/table.mjs
@@ -0,0 +1,30 @@
+import pool from './testing_db.mjs';
+
+class Customer   {
+    constructor(email) {
+        this.email = email;
+    }
+
+    
+    
+    static async getCustomerByEmail  (email, callback)  {
+        const query = 'SELECT * FROM customer WHERE email = $1'
+        try {
+            const { rows } = await pool.query(query, [email]);
+            callback(null, rows)
+          } catch (err) {
+            callback(err, null)
+          }
+    }
+
+    static async addCustomer  (email, callback)  {
+        const query = 'INSERT INTO customer (email) VALUES ($1)'
+        try {
+            const { rows } = await pool.query(query, [email]);
+            callback(null, rows)
+          } catch (err) {
+            callback(err, null)
+          }
+    }
+}
+export default Customer;
